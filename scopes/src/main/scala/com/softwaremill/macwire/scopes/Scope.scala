523de68a7a0a4e25c3feccb0c8232b82745ac46c
adamw <adam@warski.org>
2013-04-21 22:40:07 +0200

    Docs for Scope

--- scopes/src/main/scala/com/softwaremill/macwire/scopes/Scope.scala
+++ scopes/src/main/scala/com/softwaremill/macwire/scopes/Scope.scala
@@ -3,6 +3,14 @@ package com.softwaremill.macwire.scopes
 import scala.reflect.ClassTag
 
 trait Scope {
-  def apply[T](t: => T)(implicit tag: ClassTag[T]): T
+  /**
+   * Create a scoped value with the given factory class.
+   */
+  def apply[T](createT: => T)(implicit tag: ClassTag[T]): T
+
+  /**
+   * Get an instance from the current scope, for the specified key, using the given factory class to create an instance
+   * if not yet created.
+   */
   def get[T](key: String, createT: => T): T
 }
