76d9dbf4a57955839d1d8cf331a2d6433b8da5ce
adamw <adam@warski.org>
2013-04-24 17:00:11 +0200

    Using scopes

--- examples/scalatra/src/main/scala/com/softwaremill/macwire/examples/scalatra/ServletModule.scala
+++ examples/scalatra/src/main/scala/com/softwaremill/macwire/examples/scalatra/ServletModule.scala
@@ -1,8 +1,12 @@
 package com.softwaremill.macwire.examples.scalatra
 
 import com.softwaremill.macwire.examples.scalatra.logic.LogicModule
+import com.softwaremill.macwire.scopes.ThreadLocalScope
 
 trait ServletModule extends LogicModule {
   lazy val servlet1 = wire[Servlet1]
   lazy val authServlet = wire[AuthServlet]
+
+  def session = new ThreadLocalScope
+  def request = new ThreadLocalScope
 }
--- examples/scalatra/src/main/scala/com/softwaremill/macwire/examples/scalatra/logic/LogicModule.scala
+++ examples/scalatra/src/main/scala/com/softwaremill/macwire/examples/scalatra/logic/LogicModule.scala
@@ -1,11 +1,15 @@
 package com.softwaremill.macwire.examples.scalatra.logic
 
 import com.softwaremill.macwire.Macwire
+import com.softwaremill.macwire.scopes.Scope
 
 trait LogicModule extends Macwire {
-  lazy val loggedInUser = wire[LoggedInUser]
-  lazy val submittedData = wire[SubmittedData]
+  lazy val loggedInUser = session(wire[LoggedInUser])
+  lazy val submittedData = request(wire[SubmittedData])
   lazy val service1 = wire[Service1]
   lazy val service2 = wire[Service2]
   lazy val service3 = wire[Service3]
+
+  def session: Scope
+  def request: Scope
 }
