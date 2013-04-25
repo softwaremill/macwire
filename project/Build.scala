ed1e71a20a4f5bc94ff83afdd36f7cff82ccab80
adamw <adam@warski.org>
2013-04-25 16:10:33 +0200

    0.3 development

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "com.softwaremill.macwire",
-    version       := "0.2",
+    version       := "0.3-SNAPSHOT",
     scalaVersion  := "2.10.1",
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
