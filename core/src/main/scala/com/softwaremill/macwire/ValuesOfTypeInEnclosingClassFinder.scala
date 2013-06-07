084de3ffab45e9d8147dd40d95da89464cf47689
adamw <adam@warski.org>
2013-06-07 20:47:06 +0200

    Re-using method

--- core/src/main/scala/com/softwaremill/macwire/ValuesOfTypeInEnclosingClassFinder.scala
+++ core/src/main/scala/com/softwaremill/macwire/ValuesOfTypeInEnclosingClassFinder.scala
@@ -14,47 +14,14 @@ private[macwire] class ValuesOfTypeInEnclosingClassFinder[C <: Context](val c: C
       case Nil => acc
       case tree :: tail => tree match {
         case ValDef(_, name, tpt, rhs) => {
-          doFind(tail, checkCandidate(name, tpt, treeToCheck(tree, rhs), acc, "val"))
+          val candidateOk = typeCheckUtil.checkCandidate(t, name, tpt, treeToCheck(tree, rhs), "val")
+          doFind(tail, if (candidateOk) name :: acc else acc)
         }
         case DefDef(_, name, _, _, tpt, rhs) => {
-          doFind(tail, checkCandidate(name, tpt, treeToCheck(tree, rhs), acc, "def"))
+          val candidateOk = typeCheckUtil.checkCandidate(t, name, tpt, treeToCheck(tree, rhs), "def")
+          doFind(tail, if (candidateOk) name :: acc else acc)
         }
         case _ => doFind(tail, acc)
-      }
-    }
-
-    def checkCandidate(name: Name, tpt: Tree, treeToCheck: Tree, acc: List[Name], candidateDebugName: String): List[Name] = {
-      debug.withBlock(s"Checking $candidateDebugName: [$name]") {
-        val rhsTpe = if (tpt.tpe != null) {
-          tpt.tpe
-        } else {
-          // Disabling macros, no to get into an infinite loop.
-          // Duplicating the tree, not to modify the original.
-          debug(s"The type is not yet available. Trying a type-check ...")
-          val calculatedType = c.typeCheck(treeToCheck.duplicate, silent = true, withMacrosDisabled = true).tpe
-          // In case of abstract definitions, when we check the tree (not the rhs), the result is in tpt.tpe. Otherwise,
-          // it's in calculatedType.
-          val result = if (tpt.tpe == null) calculatedType else tpt.tpe
-
-          if (result == NoType) {
-            debug("Type check didn't resolve to a type. Trying to type-check an expression of the given type.")
-            val result2 = typeCheckUtil.typeCheckExpressionOfType(tpt)
-
-            debug(s"Result of second type-check: [$result2]")
-
-            result2
-          } else {
-            debug(s"Result of type-check: [$result]")
-            result
-          }
-        }
-
-        if (rhsTpe <:< t && typeCheckUtil.candidateTypeOk(rhsTpe)) {
-          debug(s"Found a match in enclosing class/trait!")
-          name.encodedName :: acc
-        } else {
-          acc
-        }
       }
     }
 
