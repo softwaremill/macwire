244b94eaf29049aec51dcdee939287a34d4ceec6
adamw <adam@warski.org>
2014-08-29 11:52:12 +0200

    Updating Scala & SBT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization  := "com.softwaremill.macwire",
     version       := "0.8-SNAPSHOT",
-    scalaVersion  := "2.11.1",
+    scalaVersion  := "2.11.2",
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
