34821a9efc785f90a1f3937f5bc05366d6e17d87
Bruno Bieth <biethb@gmail.com>
2015-09-16 08:54:28 +0200

    simplify module test-case names

--- util-tests/src/test/scala/com/softwaremill/macwire/CompileTests.scala
+++ util-tests/src/test/scala/com/softwaremill/macwire/CompileTests.scala
@@ -6,7 +6,7 @@ class CompileTests extends CompileTestsSupport {
     expectedFailures = List(
       "methodWithTaggedParamsNotFound" -> List(valueNotFound("com.softwaremill.macwire.tagging.@@[Berry,Blue]")),
       "methodWithTaggedParamsAmbiguous" -> List(ambiguousResMsg("com.softwaremill.macwire.tagging.@@[Berry,Blue]"), "blueberryArg1", "blueberryArg2"),
-      "moduleAnnotationAmbiguousWithParent" -> List(ambiguousResMsg("A"), "module.a", "parentA"),
+      "moduleAmbiguousWithParent" -> List(ambiguousResMsg("A"), "module.a", "parentA"),
       "taggedNoValueWithTag" -> List(valueNotFound("com.softwaremill.macwire.tagging.@@[Berry,Blue]")))
   )
 }
