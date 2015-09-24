70da318bbac65cd33154b8f4df5b8644b41e825c
adamw <adam@warski.org>
2015-09-24 14:51:28 +0200

    Bumping version to 2.1.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object BuildSettings {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq (
     organization  := "com.softwaremill.macwire",
-    version       := "2.0.0",
+    version       := "2.1.0-SNAPSHOT",
     scalaVersion  := "2.11.7",
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
