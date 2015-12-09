8c236377bf55a2c62f2176f891705568536c9a0b
adamw <adam@warski.org>
2015-12-09 09:23:14 +0100

    2.2.1 release

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object BuildSettings {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq (
     organization  := "com.softwaremill.macwire",
-    version       := "2.2.0",
+    version       := "2.2.1",
     scalaVersion  := "2.11.7",
     // Sonatype OSS deployment
     publishTo <<= version { (v: String) =>
