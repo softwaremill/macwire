271b40fc2f99c918a2aefa46b2050609ece11aa5
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-12-27 23:56:09 +0100

    wireProps-6-injectAnnotationButNoDependencyInScope

--- macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
+++ macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
@@ -6,8 +6,8 @@ class CompileTests extends CompileTestsSupport {
 
   runTestsWith(
     expectedFailures = List(
-      "wireProps-3-missingDependency" -> List(
-        "Cannot find a value of type: [A]")
+      "wireProps-3-missingDependency" -> List("Cannot find a value of type: [A]"),
+      "wireProps-6-injectAnnotationButNoDependencyInScope" -> List("Cannot find a value of type: [C]")
     ),
     expectedWarnings = List()
   )
