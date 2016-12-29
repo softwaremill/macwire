920b5a86704e6d27e1cfb6ae28c28b63cf80e190
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-12-29 19:40:40 +0100

    wire*Actor--3-missingDependency tests

--- macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
+++ macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
@@ -7,6 +7,8 @@ class CompileTests extends CompileTestsSupport {
   runTestsWith(
     expectedFailures = List(
       "wireProps-3-missingDependency" -> List("Cannot find a value of type: [A]"),
+      "wireAnonymousActor-3-missingDependency" -> List("Cannot find a value of type: [A]"),
+      "wireActor-3-missingDependency" -> List("Cannot find a value of type: [A]"),
       "wireProps-6-injectAnnotationButNoDependencyInScope" -> List("Cannot find a value of type: [C]"),
       "wireProps-7-notActor" -> List("type arguments [NotActor] do not conform to macro method wireProps's type parameter bounds [T <: akka.actor.Actor]")
     ),
