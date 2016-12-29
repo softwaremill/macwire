74b37bb10bebce712b7033ac1983506bff151a18
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-12-29 20:17:56 +0100

    wireActor*-7 tests

--- macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
+++ macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
@@ -14,7 +14,9 @@ class CompileTests extends CompileTestsSupport {
       "wireProps-6-injectAnnotationButNoDependencyInScope" -> List("Cannot find a value of type: [C]"),
       "wireAnonymousActor-6-injectAnnotationButNoDependencyInScope" -> List("Cannot find a value of type: [C]"),
       "wireActor-6-injectAnnotationButNoDependencyInScope" -> List("Cannot find a value of type: [C]"),
-      "wireProps-7-notActor" -> List("type arguments [NotActor] do not conform to macro method wireProps's type parameter bounds [T <: akka.actor.Actor]")
+      "wireProps-7-notActor" -> List("type arguments [NotActor] do not conform to macro method wireProps's type parameter bounds [T <: akka.actor.Actor]"),
+      "wireAnonymousActor-7-notActor" -> List("type arguments [NotActor] do not conform to macro method wireAnonymousActor's type parameter bounds [T <: akka.actor.Actor]"),
+      "wireActor-7-notActor" -> List("type arguments [NotActor] do not conform to macro method wireActor's type parameter bounds [T <: akka.actor.Actor]")
     ),
     expectedWarnings = List()
   )
