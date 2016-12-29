572d1d0858be547faf8e7db18c745c9e30bba2fa
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-12-29 19:48:23 +0100

    important case when you don't have ActorRefFactory in scope

--- macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
+++ macrosAkkaTests/src/test/scala/com/softwaremill/macwire/macwireakka/CompileTests.scala
@@ -9,6 +9,8 @@ class CompileTests extends CompileTestsSupport {
       "wireProps-3-missingDependency" -> List("Cannot find a value of type: [A]"),
       "wireAnonymousActor-3-missingDependency" -> List("Cannot find a value of type: [A]"),
       "wireActor-3-missingDependency" -> List("Cannot find a value of type: [A]"),
+      "wireAnonymousActor-3.1-missingActorRefFactoryDependency" -> List("Cannot find a value of type: [akka.actor.ActorRefFactory]"),
+      "wireActor-3.1-missingActorRefFactoryDependency" -> List("Cannot find a value of type: [akka.actor.ActorRefFactory]"),
       "wireProps-6-injectAnnotationButNoDependencyInScope" -> List("Cannot find a value of type: [C]"),
       "wireProps-7-notActor" -> List("type arguments [NotActor] do not conform to macro method wireProps's type parameter bounds [T <: akka.actor.Actor]")
     ),
