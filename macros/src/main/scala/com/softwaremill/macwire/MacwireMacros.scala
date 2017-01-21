bd36eb9c313d086d02b72c018362f25097358698
pawel.panasewicz <pawel.panasewicz@gmail.com>
2017-01-21 22:12:35 +0100

    test case for situation when companion object has no 'apply' methods

--- macros/src/main/scala/com/softwaremill/macwire/MacwireMacros.scala
+++ macros/src/main/scala/com/softwaremill/macwire/MacwireMacros.scala
@@ -18,7 +18,7 @@ object MacwireMacros {
       if(constructorCrimper.constructor.isEmpty && companionCrimper.companionType.isEmpty)
         s"Cannot find a public constructor nor a companion object for [$targetType]"
       else if(companionCrimper.applies.isDefined && companionCrimper.applies.get.isEmpty)
-        s"Companion object for $targetType has no constructor methods" //TODO this is new err message, check if it's tested
+        s"Companion object for [$targetType] has no apply methods constructing target type."
       else if(companionCrimper.applies.isDefined && companionCrimper.applies.get.size > 1)
         s"No public primary constructor found for $targetType and multiple matching apply methods in its companion object were found."
       else s"Target type not supported for wiring: $targetType. Please file a bug report with your use-case."
