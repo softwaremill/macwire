7c235c683316f7b633f28ac3cb4b6163928afd2c
adamw <adam@warski.org>
2019-01-03 21:39:29 +0100

    Fix tests

--- util-tests/src/test/scala/com/softwaremill/macwire/CompileTests.scala
+++ util-tests/src/test/scala/com/softwaremill/macwire/CompileTests.scala
@@ -4,9 +4,9 @@ class CompileTests extends CompileTestsSupport {
 
   runTestsWith(
     expectedFailures = List(
-      "methodWithTaggedParamsNotFound" -> List(valueNotFound("com.softwaremill.tagging.@@[Berry,Blue]")),
-      "methodWithTaggedParamsAmbiguous" -> List(ambiguousResMsg("com.softwaremill.tagging.@@[Berry,Blue]"), "blueberryArg1", "blueberryArg2"),
+      "methodWithTaggedParamsNotFound" -> List(valueNotFound("Berry @@ Blue")),
+      "methodWithTaggedParamsAmbiguous" -> List(ambiguousResMsg("Berry @@ Blue"), "blueberryArg1", "blueberryArg2"),
       "moduleAmbiguousWithParent" -> List(ambiguousResMsg("A"), "module.a", "parentA"),
-      "taggedNoValueWithTag" -> List(valueNotFound("com.softwaremill.tagging.@@[Berry,Blue]")))
+      "taggedNoValueWithTag" -> List(valueNotFound("Berry @@ Blue")))
   )
 }
