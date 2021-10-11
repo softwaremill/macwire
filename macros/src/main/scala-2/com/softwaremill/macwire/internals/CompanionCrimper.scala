79f752c2861dc0249bd96c3dc87eb128deb3dc9b
Mateusz Borek <mateusz.borek@softwaremill.com>
2021-10-11 21:34:17 +0200

    Apply CompanionCrimper changes

--- macros/src/main/scala-2/com/softwaremill/macwire/internals/CompanionCrimper.scala
+++ macros/src/main/scala-2/com/softwaremill/macwire/internals/CompanionCrimper.scala
@@ -2,45 +2,62 @@ package com.softwaremill.macwire.internals
 
 import scala.reflect.macros.blackbox
 
-private[macwire] class CompanionCrimper [C <: blackbox.Context, T: C#WeakTypeTag](val c: C, log: Logger) {
+private[macwire] class CompanionCrimper[C <: blackbox.Context, T: C#WeakTypeTag](val c: C, log: Logger) {
   import c.universe._
 
   type DependencyResolverType = DependencyResolver[c.type, Type, Tree]
 
   lazy val targetType: Type = implicitly[c.WeakTypeTag[T]].tpe
 
-  lazy val companionType: Option[Type] = if(targetType.companion == NoType) None else Some(targetType.companion)
+  lazy val companionType: Option[Type] = CompanionCrimper.companionType(c)(targetType)
 
-  def isCompanionApply(method: Symbol): Boolean =
+  lazy val applies: Option[List[Symbol]] = CompanionCrimper.applies(c, log)(targetType)
+  
+  def applyTree(dependencyResolver: DependencyResolverType): Option[Tree] = CompanionCrimper.applyTree[C](c, log)(targetType, dependencyResolver.resolve(_, _))
+
+}
+
+object CompanionCrimper {
+  private def showApply[C <: blackbox.Context](c: C)(s: c.Symbol): String = s.asMethod.typeSignature.toString
+
+  private def isCompanionApply[C <: blackbox.Context](c: C)(targetType: c.Type, method: c.Symbol): Boolean =
     method.isMethod &&
     method.isPublic &&
     method.asMethod.returnType <:< targetType &&
     method.asMethod.name.decodedName.toString == "apply"
 
-  lazy val applies: Option[List[Symbol]] = log.withBlock("Looking for apply methods of Companion Object") {
-    val as: Option[List[Symbol]] = companionType.map(_.members.filter(isCompanionApply).toList)
-    as.foreach(x => log.withBlock(s"There are ${x.size} apply methods:" ) { x.foreach(c => log(showApply(c))) })
+  private def companionType[C <: blackbox.Context](c: C)(targetType: c.Type): Option[c.Type] = {
+    import c.universe._
+
+    if(targetType.companion == NoType) None else Some(targetType.companion)
+  }
+
+  private def applies[C <: blackbox.Context](c: C, log: Logger)(targetType: c.Type): Option[List[c.Symbol]] = log.withBlock("Looking for apply methods of Companion Object") {
+    val as: Option[List[c.Symbol]] = companionType(c)(targetType).map(_.members.filter(CompanionCrimper.isCompanionApply(c)(targetType, _)).toList)
+    as.foreach(x => log.withBlock(s"There are ${x.size} apply methods:" ) { x.foreach(s => log(showApply(c)(s))) })
     as
   }
 
-  lazy val apply: Option[Symbol] = applies.flatMap( _ match {
-    case applyMethod :: Nil => Some(applyMethod)
-    case _ => None
-  })
+  def applyTree[C <: blackbox.Context](c: C, log: Logger)(targetType: c.Type, resolver: (c.Symbol, c.Type) => c.Tree): Option[c.Tree] = {
+    import c.universe._
 
-  lazy val applySelect: Option[Select] = apply.map(a => Select(Ident(targetType.typeSymbol.companion), a))
+    lazy val apply: Option[Symbol] = CompanionCrimper.applies(c, log)(targetType).flatMap( _ match {
+      case applyMethod :: Nil => Some(applyMethod)
+      case _ => None
+    })
 
-  lazy val applyParamLists: Option[List[List[Symbol]]] = apply.map(_.asMethod.paramLists)
+    lazy val applySelect: Option[Select] = apply.map(a => Select(Ident(targetType.typeSymbol.companion), a))
 
-  def wireParams(dependencyResolver: DependencyResolverType)(paramLists: List[List[Symbol]]): List[List[Tree]] = paramLists.map(_.map(p => dependencyResolver.resolve(p, p.typeSignature)))
+    lazy val applyParamLists: Option[List[List[Symbol]]] = apply.map(_.asMethod.paramLists)
 
-  def applyArgs(dependencyResolver: DependencyResolverType): Option[List[List[Tree]]] = applyParamLists.map(wireParams(dependencyResolver))
+    def wireParams(paramLists: List[List[Symbol]]): List[List[Tree]] = paramLists.map(_.map(p => resolver(p, p.typeSignature)))
 
-  def applyTree(dependencyResolver: DependencyResolverType): Option[Tree] = for {
-    pl: List[List[Tree]] <- applyArgs(dependencyResolver)
-    applyMethod: Tree <- applySelect
-  } yield pl.foldLeft(applyMethod)((acc: Tree, args: List[Tree]) => Apply(acc, args))
+    def applyArgs: Option[List[List[Tree]]] = applyParamLists.map(x => wireParams(x))
 
-  def showApply(c: Symbol): String = c.asMethod.typeSignature.toString
+    for {
+      pl: List[List[Tree]] <- applyArgs
+      applyMethod: Tree <- applySelect
+    } yield pl.foldLeft(applyMethod)((acc: Tree, args: List[Tree]) => Apply(acc, args))
+  }
+
 }
-
