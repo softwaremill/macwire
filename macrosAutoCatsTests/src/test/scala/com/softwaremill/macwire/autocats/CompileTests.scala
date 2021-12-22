2f3e465153956e958fe9b77b0879c08a3b20eb59
Mateusz Borek <mateusz.borek@softwaremill.com>
2021-12-22 19:14:30 +0100

    Test for at least one wire step

--- macrosAutoCatsTests/src/test/scala/com/softwaremill/macwire/autocats/CompileTests.scala
+++ macrosAutoCatsTests/src/test/scala/com/softwaremill/macwire/autocats/CompileTests.scala
@@ -6,11 +6,12 @@ class CompileTests extends CatsAutowireCompileTestsSupport {
 
   runTestsWith(
     expectedFailures = List(
-      "simpleMissingDeps" -> List("Cannot find a value of type: [B]"), //TODO List("Cannot find a value of type: [String], path: B.s")
-      "simpleMissingMultiLevelDeps" -> List("Cannot find a value of type: [C]"), //TODO List("Cannot find a value of type: [String], path: C.b.a.s")
+      "simpleMissingDeps" -> List("Cannot construct an instance of type: [B]"), //TODO List("Cannot find a value of type: [String], path: B.s")
+      "simpleMissingMultiLevelDeps" -> List("Cannot construct an instance of type: [C]"), //TODO List("Cannot find a value of type: [String], path: C.b.a.s")
       "notUsedProvider" -> List("Not used providers for the following types [D]"),
       "ambiguousInstances" -> List("Ambiguous instances of types [A]"),
-      "ambiguousTraitImplementations" -> List("Ambiguous instances of types [A, B]")
+      "ambiguousTraitImplementations" -> List("Ambiguous instances of types [A, B]"),
+      "constructInputProvider" -> List("Cannot construct an instance of type: [A]")
     ),
     expectedWarnings = List()
   )
