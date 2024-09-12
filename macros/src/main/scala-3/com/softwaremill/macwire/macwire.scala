265e8cbfaff1583609619e5f345b28670da7d25c
adamw <adam@warski.org>
2024-09-12 18:28:13 +0200

    docs

--- macros/src/main/scala-3/com/softwaremill/macwire/macwire.scala
+++ macros/src/main/scala-3/com/softwaremill/macwire/macwire.scala
@@ -3,7 +3,7 @@ package com.softwaremill.macwire
 import com.softwaremill.macwire.internals.autowire.autowireImpl
 import com.softwaremill.macwire.internals.MacwireMacros
 
-/** Wire an instance of type `T`, using the given dependencies.
+/** Create an instance of type `T`, using the given dependencies.
   *
   * Each dependency might be:
   *   - an instance to use
@@ -11,12 +11,24 @@ import com.softwaremill.macwire.internals.MacwireMacros
   *   - a class to instantiate to provide a depedency for the types it implements (provided as: `classOf[SomeType]`)
   *   - a `membersOf(instance)` call, to use the members of the given instance as dependencies
   *
-  * Any missing dependenciess will be created using the publicly available primary constructors, or apply methods.
+  * Any missing dependenciess will be created using the publicly available primary constructors or apply methods.
+  *
+  * @see
+  *   [[wire]] for a version which uses the context, within which it is called, to find dependencies.
   */
 inline def autowire[T](inline dependencies: Any*): T = ${ autowireImpl[T]('dependencies) }
 
+/** Create an instance of type `T`, using the dependencies that can be found in the context. Any value available in the
+  * surrounding trait/class/object, as well as any inherited values, are eligible to be used as dependencies.
+  *
+  * Type `T` will be instanatiated using the publicly available primary constructor or apply method.
+  *
+  * @see
+  *   [[autowire]] for a context-free version, which uses explicitly provided dependencies.
+  */
 inline def wire[T]: T = ${ MacwireMacros.wireImpl[T] }
 
+/** Collect all instances of the given type, available in the surrounding context (trait/class/object). */
 inline def wireSet[T]: Set[T] = ${ MacwireMacros.wireSet_impl[T] }
 
 inline def wireWith[RES](inline factory: () => RES): RES = ${ MacwireMacros.wireWith_impl[RES]('factory) }
@@ -84,6 +96,18 @@ inline def wireWith[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, 
 
 def wiredInModule(in: AnyRef): Wired = ???
 
+/** Create an instance of type `T`, using the dependencies that can be found in the context. Any value available in the
+  * surrounding trait/class/object, as well as any inherited values, are eligible to be used as dependencies.
+  *
+  * Type `T` will be instanatiated using the publicly available primary constructor or apply method.
+  *
+  * Any missing dependencies will be created using the publicly available primary constructors or apply methods.
+  *
+  * @see
+  *   [[wire]] for a version which doesn't automatically try to create missing dependencies.
+  * @see
+  *   [[autowire]] for a context-free version, which uses explicitly provided dependencies.
+  */
 inline def wireRec[T]: T = ${ MacwireMacros.wireRecImpl[T] }
 
 /** Marker method to be used in [[autowire]], to specify that values defined in the given value should be used for
